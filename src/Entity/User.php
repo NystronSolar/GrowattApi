5435b2ef700c4b13f5f373d6393cb88eaa90f5c4
TheDevick <erick@bilhalba.com.br>
2023-04-30 05:27:08 +0000

    feat: create User Entity

---
+++ src/Entity/User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace NystronSolar\GrowattApi\Entity;
+
+use NystronSolar\GrowattApi\ApiClient;
+
+class User
+{
+    private int $id;
+
+    private string $name;
+
+    private string $email;
+
+    private string $telephone;
+
+    private \DateTimeInterface $registerTime;
+
+    public function __construct(int $id, string $name, string $email, string $telephone, string $registerTime)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->telephone = $telephone;
+
+        $registerTimeObj = \DateTimeImmutable::createFromFormat(
+            ApiClient::TIME_FORMAT,
+            $registerTime
+        );
+
+        if (!$registerTimeObj) {
+            throw new \Exception(sprintf('Time Format cannot be created with "%s" format and "%s" value', ApiClient::TIME_FORMAT, $registerTime));
+        }
+
+        $this->registerTime = $registerTimeObj;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getTelephone(): string
+    {
+        return $this->telephone;
+    }
+
+    public function getRegisterTime(): \DateTimeInterface
+    {
+        return $this->registerTime;
+    }
+}
