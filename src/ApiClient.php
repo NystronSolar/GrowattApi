e05c0f1025aee661b6fe47f96ab78f69e1a2fa89
TheDevick <erick@bilhalba.com.br>
2023-04-30 05:29:24 +0000

    feat: create Api Client Class

---
+++ src/ApiClient.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace NystronSolar\GrowattApi;
+
+use GuzzleHttp\Client as GuzzleClient;
+use NystronSolar\GrowattApi\Response\ApiResponse;
+use NystronSolar\GrowattApi\Route\ApiRoute;
+
+class ApiClient
+{
+    /** @var string */
+    public const DEFAULT_API_URL = 'https://openapi.growatt.com';
+
+    /** @var string */
+    public const DEFAULT_API_URL_TEST = 'https://test.growatt.com';
+
+    /** @var string */
+    public const DEFAULT_API_TOKEN_TEST = '6eb6f069523055a339d71e5b1f6c88cc';
+
+    /** @var string */
+    public const DATE_FORMAT = 'Y-m-d';
+
+    /** @var string */
+    public const TIME_FORMAT = 'Y-m-d H:i:s';
+
+    /** @var string */
+    public const API_VERSION = 'v1';
+
+    private string $apiToken;
+
+    private string $apiUrl;
+
+    private GuzzleClient $guzzleClient;
+
+    public function __construct(#[\SensitiveParameter] string $apiToken, string $apiUrl = null, GuzzleClient $guzzleClient = null)
+    {
+        $this->apiToken = $apiToken;
+        $this->apiUrl = $apiUrl ?? static::DEFAULT_API_URL;
+        $this->guzzleClient = $guzzleClient ?? new GuzzleClient();
+    }
+
+    public function generateUrl(ApiRoute $apiRoute): string
+    {
+        $apiUrl = $this->getApiUrl();
+        $apiVersion = self::API_VERSION;
+        $route = $apiRoute->getRequestRoute();
+
+        return sprintf('%s/%s/%s', $apiUrl, $apiVersion, $route);
+    }
+
+    public function generateOptions(array $headers): array
+    {
+        $headers['token'] = $this->getApiToken();
+        $options = ['headers' => $headers];
+
+        return $options;
+    }
+
+    public function makeRequest(ApiRoute $apiRoute, array $parameters = []): ApiResponse|false
+    {
+        $options = $this->generateOptions($parameters);
+        $apiRouteMethod = $apiRoute->getRequestMethod();
+        $apiResponseClass = $apiRoute->getApiResponse();
+        $route = $this->generateUrl($apiRoute);
+        $client = $this->getGuzzleClient();
+
+        $response = $client->request($apiRouteMethod, $route, $options);
+        $responseJson = json_decode($response->getBody()->getContents());
+
+        if (!is_object($responseJson)) {
+            return false;
+        }
+
+        $apiResponse = $apiResponseClass::generate($responseJson);
+
+        return $apiResponse;
+    }
+
+    public function getApiToken(): string
+    {
+        return $this->apiToken;
+    }
+
+    public function getApiUrl(): string
+    {
+        return $this->apiUrl;
+    }
+
+    public function setApiUrl(string $apiUrl): self
+    {
+        $this->apiUrl = $apiUrl;
+
+        return $this;
+    }
+
+    public function getGuzzleClient(): GuzzleClient
+    {
+        return $this->guzzleClient;
+    }
+}
